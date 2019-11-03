3d4558c1454d4667f3fb5c99c9100b033e2978d8
samuelobe <48385238+samuelobe@users.noreply.github.com>
2019-11-03 01:28:27 -0500

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -5,55 +5,82 @@ from cloudant.error import CloudantException
 from cloudant.result import Result, ResultByKey
 from twilio.rest import Client
 
-def db_check():
+def db_connect():
+    client = Cloudant.iam("4936a8b9-e57c-4de5-b14b-847be444e187-bluemix", "dVyyF4i1Cs2NvTwmzlJiGHnyGlVcHm_c16LzIcOrZIH0")
+    client.connect()
+    database_name = "test"
+    return client
+
+def db_get():
     client = Cloudant.iam("4936a8b9-e57c-4de5-b14b-847be444e187-bluemix", "dVyyF4i1Cs2NvTwmzlJiGHnyGlVcHm_c16LzIcOrZIH0")
     client.connect()
     database_name = "test"
     my_database = client[database_name]
 
-    sample_data = [
-        [1, "one", "boiling", 100],
-        [2, "two", "hot", 40],
-        [3, "three", "warm", 20],
-        [4, "four", "cold", 10],
-        [5, "five", "freezing", 0]
-    ]
+    for doc in my_database:
+        print(doc['lastLat'], doc['lastLng'])
 
-    for document in sample_data:
-        # Retrieve the fields in each row.
-        number = document[0]
-        name = document[1]
-        description = document[2]
-        temperature = document[3]
 
-        # Create a JSON document that represents
-        # all the data in the row.
-        json_document = {
-            "numberField": number,
-            "nameField": name,
-            "descriptionField": description,
-            "temperatureField": temperature
-        }
-
-        # Create a document using the Database API.
-        new_document = my_database.create_document(json_document)
-
-    print('Data uploaded to database')
     client.disconnect()
+
+def db_push():
+    client = Cloudant.iam("4936a8b9-e57c-4de5-b14b-847be444e187-bluemix", "dVyyF4i1Cs2NvTwmzlJiGHnyGlVcHm_c16LzIcOrZIH0")
+    client.connect()
+    database_name = "test"
+    my_database = client[database_name]
+
+    data = {
+        'username':'skrt@gmail.com',
+        'password': 'julia1738',
+        'preferences': {
+            'data_permission': 'true',
+            'family_numbers':[{'8455210416':'Bob'},{'8455210415':'Sally'}]
+        },
+        'coordinates': [
+            [
+                {"lat": 69},
+                {"lng": 69},
+                {"timestamp":"11/3/2019"}
+            ]
+        ]
+    }
+    data['coordinates'].append([
+                {"lat": 69},
+                {"lng": 69},
+                {"timestamp":"11/4/2019"}
+        ])
+
+    my_database.create_document(data)
+    print('Date pushed to database')
+    client.disconnect()
+
+def check_login(username, password):
+    client = Cloudant.iam("4936a8b9-e57c-4de5-b14b-847be444e187-bluemix", "dVyyF4i1Cs2NvTwmzlJiGHnyGlVcHm_c16LzIcOrZIH0")
+    client.connect()
+    database_name = "test"
+    my_database = client[database_name]
+
+    for d in my_database:
+        username_db = d["username"]
+        password_db = d["password"]
+        if username == username_db and password == password_db:
+            return True
+
+    client.disconnect()
+    return False
 
 def send_text():
     # Your Account SID from twilio.com/console
-    account_sid = "AC0da46e5fbcfc6bcce3a1a4eef28d9ef6"
+    account_sid = "AC08edac3cdeeed8e8341e114d675949f6"
     # Your Auth Token from twilio.com/console
-    auth_token  = "67a090fe998a51715ab4cbf6e6685a9f"
+    auth_token  = "78fa5f050df281597e411df17c6f0896"
 
     client = Client(account_sid, auth_token)
 
     message = client.messages.create(
-        to="+19173594555", 
-        from_="+18455210416",
-        body="Hello from Python!")
-
+        to="+18455210416", 
+        from_="+19387770709",
+        body="www.google.com/maps/place/42.72128+-73.6886784")
     print(message.sid)
 
 
@@ -66,8 +93,8 @@ def printResults(data):
         print(quakes["metadata"]["title"])
 
     for q in quakes["features"]:
-        if q["properties"]["mag"] >= 5.0:
-            print(q['geometry']['coordinates'])
+        if q["properties"]["mag"] >= 4.5:
+            print(q['geometry']['coordinates'], "and mag is:", q["properties"]["mag"])
 
 def main():
     # variable to hold the source URL
@@ -86,5 +113,7 @@ def main():
 
 if __name__ == "__main__":
     #main()
-    db_check()
+    #db_get()
     #send_text()
+    db_push()
+    check_login("skrt@gmail.com","julia1738")
